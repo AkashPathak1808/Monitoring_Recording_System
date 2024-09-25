0d911f5cf3e5bd41f2e30d8d51672aabbcb99ecd
AkashPathak1808 <akashpathak180801@gmail.com>
2024-09-25 20:04:49 +0530

    Nothing Special just change one line for test purpose

--- src/main/java/com/mon_rec_sys/component/JwtAuthenticationFilter.java
+++ src/main/java/com/mon_rec_sys/component/JwtAuthenticationFilter.java
@@ -5,11 +5,11 @@ import java.io.IOException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
-import com.mon_rec_sys.service.impl.UserDetailsServiceImpl;
 import com.mon_rec_sys.utils.JwtUtils;
 
 import jakarta.servlet.FilterChain;
@@ -20,11 +20,11 @@ import jakarta.servlet.http.HttpServletResponse;
 @Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
-	private UserDetailsServiceImpl userService;
+	private UserDetailsService userService;
 
 	private JwtUtils jwtUtils;
 
-	public JwtAuthenticationFilter(UserDetailsServiceImpl userService, JwtUtils jwtUtils) {
+	public JwtAuthenticationFilter(UserDetailsService userService, JwtUtils jwtUtils) {
 		this.userService = userService;
 		this.jwtUtils = jwtUtils;
 	}
